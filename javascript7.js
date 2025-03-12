f1315402da4df4a4dce05b011928d0427421b58f
ishqafathima <149235722+ishqafathima@users.noreply.github.com>
2025-03-12 20:57:10 +0530

    Add files via upload

---
+++ javascript7.js
@@ -0,0 +1,12 @@
+console.log("Reverse the numbers in left & right 4321-5-9876")
+let mid = parseInt(9/2) + 1
+
+for(let y=mid-1; y>=1; y--){
+	console.log(y)
+}
+	
+console.log("-" + mid + "-")
+	
+for(let z=9; z>mid; z--){
+	console.log(z)
+}
