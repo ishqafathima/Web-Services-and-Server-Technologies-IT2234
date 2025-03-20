d20e189720a413f8dfa1dc3111a6eb5129e9956b
ishqafathima <149235722+ishqafathima@users.noreply.github.com>
2025-03-20 22:49:19 +0530

    Delete Reversearray.js

--- Reversearray.js
+++
@@ -1,9 +0,0 @@
-// Reverse an array using push and pop
-let arr4 = ['a', 'b', 'c', 'd'];  // Fixed array elements (added 'c')
-let arr5 = [];
-
-while (arr4.length > 0) {
-    arr5.push(arr4.pop());  // Pop from arr4 and push into arr5
-}
-
-console.log("Reversed array:", arr5);
