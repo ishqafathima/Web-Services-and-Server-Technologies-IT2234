cd74d9f8955c5d93697c0a605dffb01f60be896f
ishqafathima <149235722+ishqafathima@users.noreply.github.com>
2025-03-20 22:53:40 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,22 @@
+//functions
+
+function PrintMsg()
+{
+	console.log("Hello js")
+}
+
+PrintMsg()
+
+function sum()
+{
+	return 5+6
+}
+
+console.log(sum())
+
+function sub(a,b)
+{
+	return a-b
+}
+
+console.log(sub(5,3))
