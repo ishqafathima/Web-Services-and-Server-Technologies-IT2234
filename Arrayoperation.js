83167eb575c83f92b0743a9e2cae8197ecf758c2
ishqafathima <149235722+ishqafathima@users.noreply.github.com>
2025-03-20 21:32:08 +0530

    Add files via upload

---
+++ Arrayoperation.js
@@ -0,0 +1,17 @@
+//Array operation
+//push and pop
+
+ //Basic push and pop operations
+let arr3 = ['a', 'b', 'd'];
+console.log(arr3);
+
+arr3.push('e');
+console.log(arr3);
+
+arr3.pop();
+console.log(arr3);
+
+
+
+	
+	
