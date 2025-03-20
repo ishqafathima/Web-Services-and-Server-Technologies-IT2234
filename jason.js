bd1b28d4b0c18dd48bcd61ced98c9ac59f463ab1
ishqafathima <149235722+ishqafathima@users.noreply.github.com>
2025-03-20 21:39:02 +0530

    Add files via upload

---
+++ jason.js
@@ -0,0 +1,14 @@
+//JASON Object
+//{key:value}
+// Define a student object
+let student = {
+    regNo: '2021/ict/126',
+    name: 'shahla',
+    age: 25,
+    course: 'ict',
+    skills: ['java', 'js', 'c++']
+};
+console.log(student);
+console.log(student.name);
+console.log(student.course);
+
