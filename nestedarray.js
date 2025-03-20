60cddfaaa4d873101b48dbd9b462ed3dffaccb2f
ishqafathima <149235722+ishqafathima@users.noreply.github.com>
2025-03-20 21:22:56 +0530

    Add files via upload

---
+++ nestedarray.js
@@ -0,0 +1,12 @@
+//print the nested array
+//[[1,2,30],[5,6], [8,5,3]]
+//foreach loop
+	
+	let arr2=[[1,2,30],[5,6],[8,5,3]]
+	
+	arr2.forEach((n)=>{
+		n.forEach((i)=>{
+		console.log(i)
+	}
+	)
+	})
