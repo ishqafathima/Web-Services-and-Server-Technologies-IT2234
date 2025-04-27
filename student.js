51f268e50452fff3bce18910c278322c2aa3d6d0
ishqafathima <149235722+ishqafathima@users.noreply.github.com>
2025-04-27 20:17:42 +0530

    Delete student.js

--- student.js
+++
@@ -1,9 +0,0 @@
-let students=[
-    {regno:'2021/ict/113',name:"Thisha",gender:'female',age:23,course:'IT'},
-    {regno:'2021/ict/114',name:"Kawya",gender:'female',age:22,course:'Bio'},
-    {regno:'2021/ict/115',name:"Izzla",gender:'female',age:24,course:'BS'},
-    {regno:'2021/ict/116',name:"John",gender:'male',age:23,course:'IT'}
-
-];
-
-module .exports=students;
